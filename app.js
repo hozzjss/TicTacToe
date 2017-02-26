b89b4cdba6fbc855a1364d507eff0fe80cac3936
hozzj <medo3578@gmail.com>
2017-02-26 14:48:40 +0200

    Added an intelligent AI not a chess master but enough to be a pain in the a**

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let PCchoice = "X",
-  userChoice = "O",
+let PCchoice,
+  userChoice,
   winner,
   availableMoves,
   moveClasses = [
@@ -14,7 +14,7 @@ let PCchoice = "X",
     ".r3c3"
   ],
   tiles = new Set(moveClasses),
-  winCases= [
+  winCases = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -28,11 +28,23 @@ let PCchoice = "X",
 
 /*////////
   funs!!
-/*////////
+/*/ ///////
+
+const getUserChoice = () => {
+  $(".board").css("display", "none");
+  $(".choice button").click(function() {
+    userChoice = $(this).data("value");
+    PCchoice = userChoice === "X" ? "O" : "X";
+    $(".choice").css("display", "none");
+    $(".board").css("display", "");
+  });
+  return true;
+};
 
 const init = (option) => {
   if (option === "reset") {
     $(".tile").text("");
+    $(".result h3").text("");
     $(".tile").off();
   }
   availableMoves = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8]);
@@ -46,37 +58,26 @@ const init = (option) => {
 //
 const endGame = (option) => {
   if (option === "win") {
-    $(".result h3").text(`Winner is ${winner}`);
+    $(".result h3").text(`${winner === userChoice ? "You" : "PC"} Won!!!!`);
     availableMoves.clear();
   } else if (option === "draw") {
     $(".result h3").text(`Draw`);
   }
-  $(".tile").click(function () {
+  availableMoves.clear();
+  $(".tile").click(function() {
     init("reset");
   });
 };
 
 const makeAMove = (index, choice) => {
-  console.log("making a move");
-  if (won()) {
-      console.log("won returned true");
-      availableMoves.clear();
-      endGame("win");
-      return false;
-  } else if (draw()) {
-    console.log("draw returned true");
-    endGame("draw");
-    return false;
-  }
   tilesContent[index] = choice;
   move(index, choice);
-};
-
-//
-const draw = () => {
-  if (tilesContent.indexOf(null) === -1) {
-    console.log("draw");
-    return true;
+  if (won()) {
+    endGame("win");
+    return false;
+  } else if (draw()) {
+    endGame("draw");
+    return false;
   }
 };
 
@@ -87,28 +88,50 @@ const move = (index, choice) => {
 };
 
 //
+const draw = () => {
+  if (tilesContent.indexOf(null) === -1) {
+    return true;
+  }
+  return false;
+};
+
+//
 const won = () => {
-  let state,
-  tc = tilesContent,
-  wc = winCases;
+  let checkEqforAll;
   for (let i = 0; i < winCases.length; i += 1) {
-    state = tc[wc[i][0]] == tc[wc[i][1]] &&
-      tc[wc[i][0]] == tc[wc[i][2]] &&
-      tc[wc[i][1]] == tc[wc[i][2]];
-    if (state && tc[wc[i][2]] !== null) {
+    checkEqforAll = tilesContent[winCases[i][0]] == tilesContent[winCases[i][1]] &&
+      tilesContent[winCases[i][0]] == tilesContent[winCases[i][2]] &&
+      tilesContent[winCases[i][1]] == tilesContent[winCases[i][2]];
+    if (checkEqforAll && tilesContent[winCases[i][2]] !== null) {
       // since they're all the same then any tile is alright
-      console.log("won");
       winner = tilesContent[winCases[i][2]];
       return winner;
     }
   }
+  return false;
 };
 
 //
 const AI = () => {
-  console.log("AI");
-  let index = Array.from(availableMoves)[Math.floor(Math.random() * availableMoves.size)];
+  let index;
+  for (let i = 0; i < winCases.length; i += 1) {
+    if (tilesContent[winCases[i][0]] == tilesContent[winCases[i][2]]) {
+      if (availableMoves.has(winCases[i][1]) && tilesContent[winCases[i][0]] == userChoice)
+        index = winCases[i][1];
+    } else if (tilesContent[winCases[i][0]] == tilesContent[winCases[i][1]]) {
+      if (availableMoves.has(winCases[i][2]) && tilesContent[winCases[i][0]] == userChoice)
+        index = winCases[i][2];
+    } else if (tilesContent[winCases[i][1]] == tilesContent[winCases[i][2]]) {
+      if (availableMoves.has(winCases[i][0]) && tilesContent[winCases[i][1]] == userChoice)
+        index = winCases[i][0];
+    }
+  }
+  if (index === null || !availableMoves.has(index)) {
+    index = Array.from(availableMoves)[Math.floor(Math.random() * availableMoves.size)];
+  }
   makeAMove(index, PCchoice);
 };
 
-init();
+if (getUserChoice()) {
+  init();
+}
